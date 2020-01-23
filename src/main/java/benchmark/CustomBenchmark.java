e15c9e5dadbf5ffc964e861651f9a806cfcbaedf
Valeh Mustafayev <valeh.mustafayev@comspace.de>
2020-01-23 08:20:53 +0100

    benchmark

---
+++ src/main/java/benchmark/CustomBenchmark.java
@@ -0,0 +1,10 @@
+package benchmark;
+
+import java.util.Random;
+
+public class CustomBenchmark {
+    public static void getExecTime() {
+        final Random random = new Random();
+        
+    }
+}
