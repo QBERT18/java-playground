265db6833953d274dbb05e24842fcd43a6c25d7f
Valeh Mustafayev <valeh.mustafayev@comspace.de>
2020-04-01 08:55:20 +0200

    clean release of problem 5 and 6 of euler project

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,5 @@
-import problem5_smallest_multiple.SmallestMultiple;
-import problem6_sum_square_difference.SumSquareDifference;
-
 public class main {
     public static void main(String[] args) {
-        System.out.println(SumSquareDifference.difference());
+
     }
 }
